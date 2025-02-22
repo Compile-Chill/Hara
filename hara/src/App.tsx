e03d63a116525642083146cf6d4465b082b047eb
Valentina Córdoba <valentinacorres18@gmail.com>
2025-02-21 20:15:08 -0500

    chore: Delete unnecessary files

--- hara/src/App.tsx
+++ hara/src/App.tsx
@@ -1,11 +1,13 @@
 import { useTranslation } from "react-i18next";
 
 function App() {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
 
   return (
     <div>
       <h1>{t("hello_world")}</h1>
+      <button onClick={() => i18n.changeLanguage("es")}>ES</button>
+      <button onClick={() => i18n.changeLanguage("en")}>EN</button>
     </div>
   );
 }
