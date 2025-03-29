532b2107eac9ae0b607b1314a4399c42e7160c87
Valentina Cordoba Restrepo <120533416+Valentinacr18@users.noreply.github.com>
2025-03-29 10:18:04 -0500

    feat(i18n): implement internationalization
    * feat(i18n): implement internationalization using middleware and cookies
    
    * refactor: i18n restructuring
    
    * fix(i18n): unnecessary lines of code and comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,2 @@
 export default function Home() {
-  return (
-    <main className="flex min-h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-600">¡Hola, mundo!</h1>
-    </main>
-  );
-}
+  }
